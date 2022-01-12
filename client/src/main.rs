721c7f2d2f517c06efd618bb0f9fc1e980853de8
David Kleingeld <noreply@davidsk.dev>
2022-01-12 14:32:59 +0100

    further attempt to minimize example

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,59 @@
-mod java_setup;
+use std::hash::{Hash, Hasher};
+use std::pin::Pin;
+
+use futures::stream::{self, BoxStream};
+use futures::{Stream, TryStreamExt};
 
 pub fn main() {
-    let sub = java_setup::sub(0);
+    iced::Subscription::from_recipe(JavaSetup {});
+}
+
+pub struct JavaSetup {}
+
+#[derive(Default)]
+pub struct State {
+    stream: Option<Pin<Box<dyn Stream<Item = Result<localjava::Progress, localjava::Error>>>>>,
+    phase: Phase,
+}
+
+impl Default for Phase {
+    fn default() -> Self {
+        Self::Download
+    }
+}
+
+pub enum Phase {
+    Download,
+    Streaming,
+}
+
+impl<H, I> iced_native::subscription::Recipe<H, I> for JavaSetup
+where
+    H: Hasher,
+{
+    type Output = ();
+
+    fn hash(&self, state: &mut H) {
+        struct Marker;
+        std::any::TypeId::of::<Marker>().hash(state);
+    }
+
+    fn stream(self: Box<Self>, _input: BoxStream<'static, I>) -> BoxStream<'static, Self::Output> {
+        Box::pin(stream::unfold(State::default(), move |state| async move {
+            match &state.phase {
+                Download => match localjava::download_stream().await {
+                    Ok(stream) => {
+                        let stream = Pin::new(Box::new(stream));
+                        state.phase = Phase::Streaming;
+                        state.stream = Some(stream);
+                        Some(((), state))
+                    }
+                    Err(error) => Some(((), state)),
+                },
+                Streaming => match state.stream.unwrap().try_next().unwrap().await {
+                    _ => todo!(),
+                },
+            }
+        }))
+    }
 }
--- localjava/src/lib.rs
+++ localjava/src/lib.rs
@@ -1,13 +1,30 @@
-pub mod download;
-pub use download::Progress;
-use futures::TryStream;
+use futures::{stream, StreamExt, TryStream, TryStreamExt};
 
 #[derive(Debug)]
 pub enum Error {
     Empty,
 }
 
-pub async fn download_stream(
-) -> Result<impl TryStream<Ok = Progress, Error = download::Error>, Error> {
-    Ok(download::unpack_stream().await.unwrap())
+pub async fn download_stream() -> Result<impl TryStream<Ok = Progress, Error = Error>, Error> {
+    Ok(unpack_stream().await.unwrap())
 }
+
+pub async fn unpack_stream() -> Result<impl TryStream<Ok = Progress, Error = Error>, Error> {
+    let state = Download {};
+
+    let stream = stream::try_unfold(state, state_machine);
+    // this is needed as try_next needs Pin<TryStream> an TryStream is
+    // not implemented for Pin<TryStream> this is due to trait aliasses
+    // not yet being stable, and will not be a problem in the future.
+    // this line of code can be removed when trait aliasses are stabalized
+    // let mut stream = stream.into_stream().boxed();
+    Ok(stream.into_stream().boxed())
+}
+
+async fn state_machine(_state: Download) -> Result<Option<(Progress, Download)>, Error> {
+    Ok(None)
+}
+
+#[derive(Clone)]
+pub struct Progress;
+struct Download {}
